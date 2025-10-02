93a1dc943f9b7e9c0b0a02a87f6dda4a6aa1bf0f
Jamie Davis <jamjam@umich.edu>
2025-10-02 15:01:38 +0200

    Added sources & models for customer_orders

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,12 @@
+with source as (
+    select * from {{ ref('customers') }}
+),
+renamed as (
+    select
+        cast(id as integer) as customer_id,
+        first_name,
+        last_name,
+        email
+    from source
+)
+select * from renamed
